4f42f43b6e9732a9ac531efd2af002d60948d6f0
Philip Kuryloski <pkuryloski@pivotal.io>
2019-06-13 12:58:32 +0200

    Refactor pending-changes/interpret

--- src/concourse_pcf_foundation_resource/query/pending_changes.clj
+++ src/concourse_pcf_foundation_resource/query/pending_changes.clj
@@ -2,17 +2,10 @@
   (:require [clojure.core.match :refer [match]]
             [clojure.spec.alpha :as s]))
 
-(defn- fresh-opsman?
-  [parsed-pending-changes-response]
-  (let [{:keys [product_changes]} parsed-pending-changes-response]
-    (and (= 1 (count product_changes))
-         (= "install" (:action (first product_changes)))
-         (= "p-bosh" (:identifier (:staged (first product_changes)))))))
-
 (defn interpret
   [parsed-pending-changes-response]
   (match [parsed-pending-changes-response]
-         [(true :<< fresh-opsman?)] :fresh-opsman
+         [{:product_changes [{:action "install" :staged {:identifier "p-bosh"}}]}] :fresh-opsman
          :else :no))
 
 (s/fdef interpret
