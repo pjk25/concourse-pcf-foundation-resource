d228612537885f3b36f93f62b3babd75e133b017
Philip Kuryloski <pkuryloski@pivotal.io>
2019-06-13 21:32:05 +0200

    Make util/only-specd work with at least some coll-of

--- src/concourse_pcf_foundation_resource/util.clj
+++ src/concourse_pcf_foundation_resource/util.clj
@@ -4,18 +4,20 @@
 
 (defn only-specd
   [spec x]
-  (match [(s/describe spec)]
-         [(['keys & r] :seq)] (let [{:keys [req req-un opt opt-un]} r]
-                                (into (reduce #(assoc %1 %2 (only-specd %2 (%2 x)))
-                                              {}
-                                              (concat req opt))
-                                      (reduce #(let [unqualified (keyword (name %2))]
-                                                 (assoc %1 unqualified (only-specd %2 (unqualified x))))
-                                              {}
-                                              (concat req-un opt-un))))
-         :else x))
+  (let [described (if (qualified-keyword? spec) (s/describe spec) spec)]
+    (match [described]
+           [(['keys & r] :seq)] (let [{:keys [req req-un opt opt-un]} r]
+                                  (into (reduce #(assoc %1 %2 (only-specd %2 (%2 x)))
+                                                {}
+                                                (concat req opt))
+                                        (reduce #(let [unqualified (keyword (name %2))]
+                                                   (assoc %1 unqualified (only-specd %2 (unqualified x))))
+                                                {}
+                                                (concat req-un opt-un))))
+           [(['coll-of i] :seq)] (mapv #(only-specd i %) x)
+           :else x)))
 
-(s/fdef only-spec
-        :args (s/cat :spec qualified-keyword?
+(s/fdef only-specd
+        :args (s/cat :spec (s/or :keyword qualified-keyword? :else any?)
                      :x any?)
         :ret any?)
--- test/concourse_pcf_foundation_resource/util_test.clj
+++ test/concourse_pcf_foundation_resource/util_test.clj
@@ -8,7 +8,7 @@
 
 (s/def ::baz int?)
 
-(s/def ::bar (s/keys :opt [::baz]))
+(s/def ::bar (s/coll-of (s/keys :opt [::baz])))
 
 (s/def ::a-map (s/keys :req [::foo] :opt-un [::bar]))
 
@@ -16,7 +16,7 @@
   (stest/instrument `util/only-specd)
 
   (is (= (util/only-specd ::a-map {::foo "foostr"
-                                   :bar {::baz 123}
+                                   :bar [{::baz 123 :other 1}]
                                    :extra "extra-val"})
          {::foo "foostr"
-          :bar {::baz 123}})))
+          :bar [{::baz 123}]})))
